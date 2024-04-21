93d8f58bb252e71d34c9f303b2d8a2170f8f8e64
Tikifire <1540763539@qq.com>
2024-04-21 11:15:14 +0800

    sync upstream

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
 #
 
 # Modify default IP
@@ -16,3 +16,9 @@ sudo apt install libfuse-dev
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/Zxilly/UA2F.git package/UA2F
 git clone https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
